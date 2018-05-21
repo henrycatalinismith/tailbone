d33413721807319342c5591c83a9f5735b47ac34
Henry Catalini Smith <henry@catalinismith.se>
2018-05-21 18:05:18 +0100

    Store highScore in localStorage

--- play.js
+++ play.js
@@ -5,11 +5,38 @@ window.addEventListener("load", () => {
   const vw = v => (v * window.innerWidth) / 100;
   const vmin = v => Math.min(vh(v), vw(v));
 
+  let highScore = JSON.parse(
+    localStorage.getItem("highScore") || null
+  );
+
   canvas.width = vmin(100);
   canvas.height = vmin(100);
 
+  const GpioArray = function () {
+    Array.call(this, 128);
+  }
+
+  GpioArray.prototype = Object.create(Array.prototype, {
+    length: {
+      value: 128,
+      writable: false,
+    }
+  });
+
+  GpioArray.prototype.constructor = GpioArray;
+
+  const persist = (hex, name) => {
+    Object.defineProperty(GpioArray.prototype, hex-0x5f80, {
+      get: () => localStorage.getItem(name),
+      set: v => localStorage.setItem(name, v),
+    });
+  }
+
+  persist(0x5f80, "highScore");
+
   window.Module = { canvas };
   window.pico8_buttons = [0];
+  window.pico8_gpio = new GpioArray();
 
   document.body.addEventListener("touchstart", () => {
     pico8_buttons[0] = 32;
@@ -23,3 +50,4 @@ window.addEventListener("load", () => {
   game.src = "tailbone.js";
   document.head.appendChild(game);
 });
+
