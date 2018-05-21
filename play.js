8fcb76b281353f2cdf164beb2ab66e10e2b86568
Henry Catalini Smith <henry@catalinismith.se>
2018-05-21 17:25:44 +0100

    Fix canvas resize troubles

--- play.js
+++ play.js
@@ -1,8 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-  window.Module = {
-    canvas: document.querySelector("canvas"),
-  };
+window.addEventListener("load", () => {
+  const canvas = document.querySelector("canvas");
 
+  const vh = v => (v * window.innerHeight) / 100;
+  const vw = v => (v * window.innerWidth) / 100;
+  const vmin = v => Math.min(vh(v), vw(v));
+
+  canvas.width = vmin(100);
+  canvas.height = vmin(100);
+
+  window.Module = { canvas };
   window.pico8_buttons = [0];
 
   document.body.addEventListener("touchstart", () => {
