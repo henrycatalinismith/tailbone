3cff116f94dea917a16e95b5219bd077f280f364
Henry Catalini Smith <henry@catalinismith.se>
2020-11-03 20:39:09 +0100

    Initial Commit

---
+++ Tailbone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tailbone
+//
+//  Created by Henry Catalini Smith on 2020-11-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tailbone/TailboneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TailboneApp.swift
+//  Tailbone
+//
+//  Created by Henry Catalini Smith on 2020-11-03.
+//
+
+import SwiftUI
+
+@main
+struct TailboneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
