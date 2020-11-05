86c8bfb23fc7a72378cc9c77adc9f954fdcb2afa
Henry Catalini Smith <henry@catalinismith.se>
2020-11-05 22:02:47 +0100

    simplify this

--- TailboneApp.swift
+++ TailboneApp.swift
@@ -83,7 +83,6 @@ struct TailboneApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
         }
     }
 }
