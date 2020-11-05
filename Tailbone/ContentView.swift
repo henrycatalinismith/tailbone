804377f6aa195427f4e98db0b074fc7e8634a4cb
Henry Catalini Smith <henry@catalinismith.se>
2020-11-05 16:43:59 +0100

    skip menu in app

--- Tailbone/ContentView.swift
+++ Tailbone/ContentView.swift
@@ -18,14 +18,32 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+class ViewController: UIViewController, WKScriptMessageHandler {
+    func userContentController(
+        _ userContentController: WKUserContentController,
+        didReceive message: WKScriptMessage
+    ) {
+        // Do nothing. This is only here to give the JS something to detect
+        // so that it can skip the web version's start menu.
+    }
+}
+
 struct Webview: UIViewRepresentable {
     let url: URL
 
     func makeUIView(
         context: UIViewRepresentableContext<Webview>
     ) -> WKWebView {
+        let contentController = WKUserContentController()
+        contentController.add(
+            ViewController.init(),
+            name: "placeholder"
+        )
+
         let config = WKWebViewConfiguration()
         config.mediaTypesRequiringUserActionForPlayback = []
+        config.userContentController = contentController
+
         let webview = WKWebView(
             frame: .zero,
             configuration: config
