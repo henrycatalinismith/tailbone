ddb929466fb5723813dadc64b3e6346b82031b67
Henry Catalini Smith <henry@catalinismith.se>
2020-11-20 12:10:15 +0100

    passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const pwa = require("eleventy-plugin-pwa")
 const fs = require("fs")
 
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("_headers")
   eleventyConfig.addPassthroughCopy("images")
   eleventyConfig.addPassthroughCopy("manifest.webmanifest")
 
