79439cab0b5ac77648b9e6d778722bbc1a9458d3
Henry Catalini Smith <henry@catalinismith.se>
2020-11-20 11:28:58 +0100

    undo eleventy bullshit

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,15 @@
+const fs = require("fs")
+
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("index.html")
+  eleventyConfig.on(
+    "afterBuild",
+    function() {
+      fs.copyFileSync(
+        "./index.html",
+        "./_site/index.html",
+      )
+    }
+  )
   eleventyConfig.addPassthroughCopy("images")
 }
 
