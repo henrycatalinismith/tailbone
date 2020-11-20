07aa06d59f4172bfeedba9223ed992f482ce5525
Henry Catalini Smith <henry@catalinismith.se>
2020-11-20 11:25:31 +0100

    remove

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,5 @@
-const sharp = require("sharp")
-
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("index.html")
   eleventyConfig.addPassthroughCopy("images")
-
-  sharp("./Assets.xcassets/AppIcon.appiconset/1024.png")
-    .resize(16)
-    .toFile(`./images/icon-16x16.png`)
-
-  sharp("./Assets.xcassets/AppIcon.appiconset/1024.png")
-    .resize(32)
-    .toFile(`./images/icon-32x32.png`)
-
-  ;([57, 180, 152, 144, 120, 114, 76, 72]).forEach(n => {
-    sharp("./Assets.xcassets/AppIcon.appiconset/1024.png")
-      .resize(n)
-      .toFile(`./images/apple-touch-icon-${n}x${n}.png`)
-  })
 }
 
