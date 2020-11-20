a60cdfb00f14bce7621763a8501ae3655245ab68
Henry Catalini Smith <henry@catalinismith.se>
2020-11-20 11:21:03 +0100

    more icons

--- .eleventy.js
+++ .eleventy.js
@@ -5,8 +5,12 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images")
 
   sharp("./Assets.xcassets/AppIcon.appiconset/1024.png")
-    .resize(192)
-    .toFile(`./images/touch-icon-192x192.png`)
+    .resize(16)
+    .toFile(`./images/icon-16x16.png`)
+
+  sharp("./Assets.xcassets/AppIcon.appiconset/1024.png")
+    .resize(32)
+    .toFile(`./images/icon-32x32.png`)
 
   ;([57, 180, 152, 144, 120, 114, 76, 72]).forEach(n => {
     sharp("./Assets.xcassets/AppIcon.appiconset/1024.png")
