5c7d550797e33e2fc7374c66876bb1f885bf9ed2
Henry Catalini Smith <henry@catalinismith.se>
2024-07-28 19:01:26 +0200

    Fix build

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,19 @@
-const pwa = require("eleventy-plugin-pwa")
-const fs = require("fs")
+import fs from "fs"
 
-module.exports = function(eleventyConfig) {
+export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("_headers")
   eleventyConfig.addPassthroughCopy("images/*.png")
   eleventyConfig.addPassthroughCopy("manifest.webmanifest")
   eleventyConfig.addPassthroughCopy("videos")
 
-  eleventyConfig.addPlugin(pwa, {
-    swDest: "./_site/sw.js",
-  })
-
   eleventyConfig.on(
     "afterBuild",
     function() {
       fs.copyFileSync(
-        "./index.html",
-        "./_site/index.html",
+        "./tailbone.html",
+        "./_site/tailbone.html",
       )
-      fs.mkdirSync("./_site/disk2")
+      fs.mkdirSync("./_sitedisk2")
       fs.copyFileSync(
         "./disk2/index.html",
         "./_site/disk2/index.html",
